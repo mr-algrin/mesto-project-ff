2c75b929e9f433121e034f4c1b50f035fba7ad10
Alex Grin <mr.algrin@gmail.com>
2024-02-23 04:08:02 +0300

    completed project

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Проектная работа Mesto
 
+## https://github.com/mr-algrin/mesto-project-ff
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,39 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector("#card-template").content;
 
 // @todo: DOM узлы
+const cardsListContainer = document.querySelector(".places__list");
+const addCardButton = document.querySelector(".profile__add-button");
+
+if (addCardButton !== null) {
+  addCardButton.addEventListener("click", () =>
+    createCard(getRandomCard(), deleteCard)
+  );
+}
 
 // @todo: Функция создания карточки
+const createCard = (cardItem, deleteCardCallback) => {
+  if (typeof cardItem !== "object") return;
+
+  if (cardItem.name === undefined || cardItem.link === undefined) return;
+
+  const cardElement = cardTemplate.cloneNode(true);
+  cardElement.querySelector(".card__image").src = cardItem.link;
+  cardElement.querySelector(".card__title").textContent = cardItem.name;
+  cardElement
+    .querySelector(".card__delete-button")
+    .addEventListener("click", deleteCardCallback);
+  cardsListContainer.append(cardElement);
+};
 
 // @todo: Функция удаления карточки
+const deleteCard = (e) => {
+  if (e.target && e.target.parentElement) e.target.parentElement.remove();
+};
 
 // @todo: Вывести карточки на страницу
+const drawCards = () => {
+  initialCards.forEach((cardItem) => createCard(cardItem, deleteCard));
+};
+
+drawCards();
