c2fc62251fd26db6beb257458301ecc193808f4c
Alex Grin <mr.algrin@gmail.com>
2024-02-23 12:51:09 +0300

    fix issues after review

--- scripts/index.js
+++ scripts/index.js
@@ -6,34 +6,47 @@ const cardsListContainer = document.querySelector(".places__list");
 const addCardButton = document.querySelector(".profile__add-button");
 
 if (addCardButton !== null) {
-  addCardButton.addEventListener("click", () =>
-    createCard(getRandomCard(), deleteCard)
-  );
+  addCardButton.addEventListener("click", () => {
+    const card = createCard(getRandomCard(), deleteCard);
+    card !== undefined && cardsListContainer.append(card);
+  });
 }
 
 // @todo: Функция создания карточки
 const createCard = (cardItem, deleteCardCallback) => {
-  if (typeof cardItem !== "object") return;
-
-  if (cardItem.name === undefined || cardItem.link === undefined) return;
+  if (
+    typeof cardItem !== "object" ||
+    cardItem.name === undefined ||
+    cardItem.link === undefined
+  )
+    return undefined;
 
   const cardElement = cardTemplate.cloneNode(true);
-  cardElement.querySelector(".card__image").src = cardItem.link;
+  const imageElement = cardElement.querySelector(".card__image");
+  imageElement.src = cardItem.link;
+  imageElement.alt = `Изображение ${cardItem.name}`;
   cardElement.querySelector(".card__title").textContent = cardItem.name;
   cardElement
     .querySelector(".card__delete-button")
     .addEventListener("click", deleteCardCallback);
-  cardsListContainer.append(cardElement);
+  return cardElement;
 };
 
 // @todo: Функция удаления карточки
 const deleteCard = (e) => {
-  if (e.target && e.target.parentElement) e.target.parentElement.remove();
+  if (!e.target)
+    return;
+
+  const parentElement = e.target.closest('.card')
+  parentElement && parentElement.remove();
 };
 
 // @todo: Вывести карточки на страницу
 const drawCards = () => {
-  initialCards.forEach((cardItem) => createCard(cardItem, deleteCard));
+  initialCards.forEach((cardItem) => {
+    const card = createCard(cardItem, deleteCard);
+    card !== undefined && cardsListContainer.append(card);
+  });
 };
 
 drawCards();
